81a6113aa7e0e3cea938201756dfc20888cba66e
lunapon <lunapon@users.noreply.github.com>
2021-09-11 13:46:31 +0800

    測試Google Sheets商品

--- arala-test/js/ui.products.js
+++ arala-test/js/ui.products.js
@@ -10,7 +10,7 @@ function arrayToProduct(id, sheetData, sheet) {
 
   var itemDiv = '';
 
-  for (var i = 0; i < sheetData.length; i++) {
+  for (var i = 1; i < sheetData.length; i++) {
     var name = sheetData[0];
     var image = sheetData[1];
     var link = sheetData[2];
@@ -18,16 +18,16 @@ function arrayToProduct(id, sheetData, sheet) {
     var highprice = sheetData[4].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     var tag = sheetData[5];
     //HTML
-    itemDiv += '<a id="product' + sheet + '-' + (i + 1) + '"></a>';
+    itemDiv += '<a id="product' + sheet + '-' + i + '"></a>';
     itemDiv += '<figure class="product col-6 col-md-4 col-lg-2 px-1"><div class="card">';
     //image
-    itemDiv += '<a class="card-img-top gtm-product' + sheet + '-' + (i + 1) + '" href="' + link + '" target="_blank" itemprop="url">';
+    itemDiv += '<a class="card-img-top gtm-product' + sheet + '-' + i + '" href="' + link + '" target="_blank" itemprop="url">';
     itemDiv += '<div class="thumbnail rounded-sm"><div class="thumbnail-content"><img class="thumbnail-image" src="' + image + '" alt="' + name + '" itemprop="image"></div></div>';
     itemDiv += '</a>';
     //info
     itemDiv += '<figcaption class="card-body p-0">';
     itemDiv += '<div class="text-sm text-white text-center bg-dark p-1">' + tag + '</div>';
-    itemDiv += '<a class="text-gray-900 gtm-product' + sheet + '-' + (i + 1) + '" href="' + link + '" target="_blank" itemprop="url"><span class="text-ellipsis-1 text-center m-2" itemprop="name">' + name + '</span></a>';
+    itemDiv += '<a class="text-gray-900 gtm-product' + sheet + '-' + i + '" href="' + link + '" target="_blank" itemprop="url"><span class="text-ellipsis-1 text-center m-2" itemprop="name">' + name + '</span></a>';
     itemDiv += '<div class="text-price text-center m-2" itemprop="offers" aria-label="價格"><span class="text-xs mr-1" itemprop="priceCurrency" content="NTD">$</span><span class="text-3xl" itemprop="price" content="' + lowprice + '">' + lowprice + '</span>';
     if (highprice != '') {
       itemDiv += '<del class="text-xs text-gray-400 ml-1">$' + highprice + '</del></div>';
@@ -52,7 +52,7 @@ $(document).ready(function() {
         dataType: 'json',
         success: function(data) {
           console.log(data);
-          arrayToProduct(rowId, data, i);
+          arrayToProduct(rowId, data.values, i);
           jumpToCategory();
         },
         error: function() {
